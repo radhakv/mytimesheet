a91927c386354aa8add8fa371ddcb15378985948
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-07-30 10:27:46 +0000

    added squabble application

---
+++ AppEngineMain/src/moten/david/squabble/client/ApplicationService.java
@@ -0,0 +1,12 @@
+package moten.david.squabble.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("service")
+public interface ApplicationService extends RemoteService {
+
+}
---
+++ AppEngineMain/src/moten/david/squabble/server/ApplicationServiceImpl.java
@@ -0,0 +1,24 @@
+package moten.david.squabble.server;
+
+import moten.david.squabble.client.ApplicationService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class ApplicationServiceImpl extends RemoteServiceServlet implements
+		ApplicationService {
+
+	private static final long serialVersionUID = 3837567586122697633L;
+
+	public void newGame(String gameId) {
+
+	}
+
+	public void processGuess(String player, String word) {
+
+	}
+
+	public void processNextTurn(String player) {
+
+	}
+
+}
