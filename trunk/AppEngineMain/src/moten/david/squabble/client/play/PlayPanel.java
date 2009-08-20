60466d7d15662221bde610a8320d64d459be6228
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-08-20 06:41:37 +0000

    dev

--- trunk/AppEngineMain/src/moten/david/squabble/client/play/PlayPanel.java
+++ trunk/AppEngineMain/src/moten/david/squabble/client/play/PlayPanel.java
@@ -2,18 +2,39 @@ package moten.david.squabble.client.play;
 
 import moten.david.squabble.client.Application;
 import moten.david.squabble.client.controller.ControllerListener;
+import moten.david.squabble.client.event.GameSelected;
 import moten.david.squabble.client.event.NameSelected;
 
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class PlayPanel extends VerticalPanel {
 	private String name;
+	private final Label message = new Label();
+	private Long gameId;
 
 	public PlayPanel() {
-		add(new HTML("<p>game</p>"));
+
+		add(message);
+		updateStatus();
 		Application.getInstance().getController().addListener(
 				NameSelected.class, createNameSelectedListener());
+		Application.getInstance().getController().addListener(
+				GameSelected.class, createGameSelectedListener());
+	}
+
+	private ControllerListener<GameSelected> createGameSelectedListener() {
+		return new ControllerListener<GameSelected>() {
+			@Override
+			public void event(GameSelected event) {
+				gameId = event.getGameId();
+				updateStatus();
+			}
+		};
+	}
+
+	protected void updateStatus() {
+		message.setText(name + " playing game " + gameId);
 	}
 
 	private ControllerListener<NameSelected> createNameSelectedListener() {
@@ -22,6 +43,7 @@ public class PlayPanel extends VerticalPanel {
 			@Override
 			public void event(NameSelected event) {
 				name = event.getName();
+				updateStatus();
 			}
 		};
 	}
