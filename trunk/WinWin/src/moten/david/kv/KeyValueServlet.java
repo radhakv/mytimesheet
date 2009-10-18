353443ba2a8b06b26c02df288599eeca4220c223
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-10-18 22:44:42 +0000

    dev

--- trunk/WinWin/src/moten/david/kv/KeyValueServlet.java
+++ trunk/WinWin/src/moten/david/kv/KeyValueServlet.java
@@ -47,50 +47,57 @@ public class KeyValueServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String key = request.getParameter("key");
-		String value = request.getParameter("value");
-		if (key == null)
-			throw new ServletException("key parameter must not be null");
-		String action = request.getParameter("action");
-		if (request.getCharacterEncoding() != null) {
-			key = new String(key.getBytes(), request.getCharacterEncoding());
-			value = new String(value.getBytes(), request.getCharacterEncoding());
-			action = new String(action.getBytes(), request
-					.getCharacterEncoding());
-		}
-		// there are some key patterns that cause special behaviour
-		// key starts with secure
-		checkSecure(key);
-		checkSecurePassword(key);
-		checkAuthenticated(key, request);
+		try {
+			String key = request.getParameter("key");
+			String value = request.getParameter("value");
+			if (key == null)
+				throw new ServletException("key parameter must not be null");
+			String action = request.getParameter("action");
+			if (request.getCharacterEncoding() != null) {
+				key = new String(key.getBytes(), request.getCharacterEncoding());
+				value = new String(value.getBytes(), request
+						.getCharacterEncoding());
+				action = new String(action.getBytes(), request
+						.getCharacterEncoding());
+			}
+			// there are some key patterns that cause special behaviour
+			// key starts with secure
+			checkSecure(key);
+			checkSecurePassword(key);
+			checkAuthenticated(key, request);
 
-		if ("get".equals(action)) {
-			String contentType = request.getParameter("contentType");
-			if (contentType != null)
-				response.setContentType(contentType);
-			String result = keyValueService.get(key);
-			String filename = request.getParameter("filename");
-			if (filename != null)
-				response.setHeader("Content-Disposition", "inline; filename="
-						+ filename);
-			if ("true".equalsIgnoreCase(request.getParameter("decodeB64")))
-				// decode B64 to bytes
-				respondWith(response, Base64.toBytes(result));
-			else
-				respondWith(response, (result == null ? "" : result));
-		} else if ("put".equals(action)) {
-			if (value == null)
-				throw new ServletException("value parameter must not be null");
-			keyValueService.put(key, value);
-			respondWith(response, "ok");
-		} else if ("append".equals(action)) {
-			if (value == null)
-				throw new ServletException("value parameter must not be null");
-			keyValueService.append(key, value);
-			respondWith(response, "ok");
-		} else
-			throw new ServletException(
-					"action parameter must be either 'get' or 'put'");
+			if ("get".equals(action)) {
+				String contentType = request.getParameter("contentType");
+				if (contentType != null)
+					response.setContentType(contentType);
+				String result = keyValueService.get(key);
+				String filename = request.getParameter("filename");
+				if (filename != null)
+					response.setHeader("Content-Disposition",
+							"inline; filename=" + filename);
+				if ("true".equalsIgnoreCase(request.getParameter("decodeB64")))
+					// decode B64 to bytes
+					respondWith(response, Base64.toBytes(result));
+				else
+					respondWith(response, (result == null ? "" : result));
+			} else if ("put".equals(action)) {
+				if (value == null)
+					throw new ServletException(
+							"value parameter must not be null");
+				keyValueService.put(key, value);
+				respondWith(response, "ok");
+			} else if ("append".equals(action)) {
+				if (value == null)
+					throw new ServletException(
+							"value parameter must not be null");
+				keyValueService.append(key, value);
+				respondWith(response, "ok");
+			} else
+				throw new ServletException(
+						"action parameter must be either 'get' or 'put'");
+		} catch (ServletException e) {
+			respondWith(response, e.getMessage());
+		}
 	}
 
 	private void checkAuthenticated(String key, HttpServletRequest request)
@@ -101,7 +108,7 @@ public class KeyValueServlet extends HttpServlet {
 		// instance do not allow cookies
 		if (key.startsWith(AUTHENTICATED)
 				&& key.length() > AUTHENTICATED.length()) {
-			if (!request.getProtocol().equals("https")) {
+			if (!request.getScheme().equals("https")) {
 				throw new ServletException(
 						"as this key starts with '"
 								+ AUTHENTICATED
