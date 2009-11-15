e4dd0420ef194bb874e55caaa7187d5d81c24ad6
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-11-15 03:51:27 +0000

    dev

--- trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/ProgrammePanel.java
+++ trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/ProgrammePanel.java
@@ -15,6 +15,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
@@ -37,15 +38,16 @@ public class ProgrammePanel extends VerticalPanel {
 				ShowProgramme.class, createRefreshListener());
 		getProgrammeCallback = createGetProgrammeCallback();
 		table = new FlexTable();
-		table.setStyleName("entries");
-		channels.add("ABC-Can");
-		channels.add("ABC2");
+		table.setStyleName("programme");
+
 		channels.add("SBS-Can");
 		channels.add("SBSTWO-NSW");
+		channels.add("One-NSW");
+		channels.add("ABC-Can");
+		channels.add("ABC2");
 		channels.add("Prime-Can");
 		channels.add("Ten-Can");
 		channels.add("WIN-Can");
-		channels.add("One-NSW");
 		channels.add("GO");
 	}
 
@@ -61,45 +63,99 @@ public class ProgrammePanel extends VerticalPanel {
 			public void onSuccess(MyProgrammeItem[] items) {
 				try {
 					Date now = new Date();
-					int totalExtraSpan = 0;
-					for (final MyProgrammeItem item : items) {
-						int row = channels.indexOf(item.getChannelId());
-						table.setText(row, 0, item.getChannelId());
-						int col = item.getStartTimeInMinutes() / 5 + 1;
-						col -= totalExtraSpan;
-						int span = (item.getStopTimeInMinutes() - item
-								.getStartTimeInMinutes()) / 5;
-						totalExtraSpan += span - 1;
 
-						String startTime = (item.getStartTimeInMinutes() / 60)
-								+ "";
-						if (startTime.length() == 1)
-							startTime = "0" + startTime;
-						startTime += ":";
-						String minutes = (item.getStartTimeInMinutes() % 60)
-								+ "";
-						if (minutes.length() == 1)
-							minutes = "0" + minutes;
-						startTime += minutes;
-						VerticalPanel vp = new VerticalPanel();
-						vp.setStyleName("noBorder");
-						Label labelTime = new Label(startTime);
-						if (item.getStart().before(now)
-								&& item.getStop().after(now))
-							labelTime.setStyleName("current");
-						vp.add(labelTime);
-						ClickHandler clickHandler = createClickHandler(item
-								.getChannelId());
-						labelTime.addClickHandler(clickHandler);
-						Label labelTitle = new Label(item.getTitle());
-						labelTitle.addClickHandler(clickHandler);
-						vp.add(labelTitle);
-						table.setWidget(row, col, vp);
-						// table.getFlexCellFormatter().setStyleName(row, col,
-						// "entry");
-						table.getFlexCellFormatter().setColSpan(row, col, span);
+					// int nowMinutes = now.getHours() * 60 + now.getMinutes();
+					int totalExtraSpan = 0;
+					String lastChannelId = null;
+					Integer bestStartTimeMinutes = null;
+					for (final MyProgrammeItem item : items) {
+						if (bestStartTimeMinutes == null
+								|| item.getStartTimeInMinutes() < bestStartTimeMinutes)
+							bestStartTimeMinutes = item.getStartTimeInMinutes();
+					}
+					if (bestStartTimeMinutes == null)
+						bestStartTimeMinutes = 0;
+
+					for (final MyProgrammeItem item : items) {
+						if (item.getStartTimeInMinutes() >= bestStartTimeMinutes) {
+							if (!item.getChannelId().equals(lastChannelId))
+								totalExtraSpan = 0;
+							int row = channels.indexOf(item.getChannelId());
+							table.setText(row, 0, item.getChannelId());
+							int col = (item.getStartTimeInMinutes() - bestStartTimeMinutes) / 5 + 1;
+							col -= totalExtraSpan;
+							int span = (item.getStopTimeInMinutes() - item
+									.getStartTimeInMinutes()) / 5;
+							totalExtraSpan += span - 1;
+
+							String startTime = (item.getStartTimeInMinutes() / 60)
+									+ "";
+							if (startTime.length() == 1)
+								startTime = "0" + startTime;
+							startTime += ":";
+							String minutes = (item.getStartTimeInMinutes() % 60)
+									+ "";
+							if (minutes.length() == 1)
+								minutes = "0" + minutes;
+							startTime += minutes;
+							{
+								VerticalPanel vp = new VerticalPanel();
+								vp.setStyleName("noBorder");
+								Label labelTime = new Label(startTime);
+								if (item.getStart().before(now)
+										&& item.getStop().after(now))
+									labelTime.setStyleName("currentTime");
+								else
+									labelTime.setStyleName("time");
+								vp.add(labelTime);
+								ClickHandler clickHandler = createClickHandler(item
+										.getChannelId());
+								labelTime.addClickHandler(clickHandler);
+								Label labelTitle = new Label(item.getTitle());
+								labelTitle.setStyleName("itemTitle");
+								labelTitle.addClickHandler(clickHandler);
+
+								vp.add(labelTitle);
+								if (true || item.getStopTimeInMinutes()
+										- item.getStartTimeInMinutes() > 60) {
+									Label text = new Label();
+									String desc = item.getDescription();
+									// if (desc != null && desc.length() > 250)
+									// desc = desc.substring(0, 250);
+									text.setText(desc);
+									text.setStyleName("itemDescription");
+									DisclosurePanel d = new DisclosurePanel();
+									d.setContent(text);
+									d.setHeader(new Label("..."));
+									vp.add(d);
+									text.addClickHandler(clickHandler);
+								}
+
+								table.setWidget(row, col, vp);
+							}
+							table.getFlexCellFormatter().setColSpan(row, col,
+									span);
+							table.getRowFormatter().setStyleName(row,
+									"programmeRow");
+							table.getFlexCellFormatter().setStyleName(row, col,
+									"programmeItem");
+							lastChannelId = item.getChannelId();
+						}
 
 					}
+
+					for (int row = 0; row < table.getRowCount(); row++) {
+						int span = 1;
+						int col = 1;
+						while (table.getWidget(row, col) == null
+								&& col < table.getCellCount(row)) {
+							table.getFlexCellFormatter().setStyleName(row, col,
+									"programmeItemEmpty");
+							span++;
+							col++;
+						}
+					}
+
 				} catch (RuntimeException e) {
 					add(new Label(e.toString()));
 				}
@@ -144,9 +200,9 @@ public class ProgrammePanel extends VerticalPanel {
 		clear();
 		table.clear();
 		add(table);
-		for (String channelId : channels)
-			applicationService.getProgramme(channelId, new Date(),
-					getProgrammeCallback);
-
+		Date now = new Date();
+		applicationService.getProgramme(channels.toArray(new String[] {}), now,
+				new Date(now.getTime() + 24 * 3600 * 1000),
+				getProgrammeCallback);
 	}
 }
