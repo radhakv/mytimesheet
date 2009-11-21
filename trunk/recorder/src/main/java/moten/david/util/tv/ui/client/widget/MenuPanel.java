87e0d5752e45f8aa7e8243203fed6fbdd442ec4c
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-11-21 23:58:42 +0000

    move buttons to title panel

--- trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/MenuPanel.java
+++ trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/MenuPanel.java
@@ -1,13 +1,5 @@
 package moten.david.util.tv.ui.client.widget;
 
-import moten.david.util.tv.ui.client.Application;
-import moten.david.util.tv.ui.client.controller.ControllerListener;
-import moten.david.util.tv.ui.client.event.ProgrammeLoaded;
-import moten.david.util.tv.ui.client.event.ShowProgramme;
-
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class MenuPanel extends VerticalPanel {
@@ -15,38 +7,5 @@ public class MenuPanel extends VerticalPanel {
 	public MenuPanel() {
 
 		setStyleName("menu");
-		final Button showProgramme = createMenuItem("Programme");
-		add(showProgramme);
-		showProgramme.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				Application.getInstance().getController().event(
-						new ShowProgramme());
-			}
-		});
-		Application.getInstance().getController().addListener(
-				ShowProgramme.class, new ControllerListener<ShowProgramme>() {
-
-					@Override
-					public void event(ShowProgramme event) {
-						showProgramme.setText("Loading...");
-					}
-				});
-		Application.getInstance().getController().addListener(
-				ProgrammeLoaded.class,
-				new ControllerListener<ProgrammeLoaded>() {
-
-					@Override
-					public void event(ProgrammeLoaded event) {
-						showProgramme.setText("Programme");
-					}
-				});
 	}
-
-	private Button createMenuItem(String label) {
-		Button button = new Button(label);
-		button.setStyleName("menuItem");
-		return button;
-	}
-
 }
--- trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/TitlePanel.java
+++ trunk/recorder/src/main/java/moten/david/util/tv/ui/client/widget/TitlePanel.java
@@ -1,5 +1,13 @@
 package moten.david.util.tv.ui.client.widget;
 
+import moten.david.util.tv.ui.client.Application;
+import moten.david.util.tv.ui.client.controller.ControllerListener;
+import moten.david.util.tv.ui.client.event.ProgrammeLoaded;
+import moten.david.util.tv.ui.client.event.ShowProgramme;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class TitlePanel extends HorizontalPanel {
@@ -7,6 +15,38 @@ public class TitlePanel extends HorizontalPanel {
 	public TitlePanel() {
 		setStyleName("title");
 		// add(new HTML("<h2>TV Recorder</h2>"));
+		final Button showProgramme = createMenuItem("Programme");
+		add(showProgramme);
+		showProgramme.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Application.getInstance().getController().event(
+						new ShowProgramme());
+			}
+		});
+		Application.getInstance().getController().addListener(
+				ShowProgramme.class, new ControllerListener<ShowProgramme>() {
+
+					@Override
+					public void event(ShowProgramme event) {
+						showProgramme.setText("Loading...");
+					}
+				});
+		Application.getInstance().getController().addListener(
+				ProgrammeLoaded.class,
+				new ControllerListener<ProgrammeLoaded>() {
+
+					@Override
+					public void event(ProgrammeLoaded event) {
+						showProgramme.setText("Programme");
+					}
+				});
+	}
+
+	private Button createMenuItem(String label) {
+		Button button = new Button(label);
+		button.setStyleName("menuItem");
+		return button;
 	}
 
 }
