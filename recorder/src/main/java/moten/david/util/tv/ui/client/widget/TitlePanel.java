f21c967c94f9a98b3f58beafcb51f5256f9174eb
davidmoten <davidmoten@479e2da2-6ac8-11de-a84c-2f1c77ec4fbd>
2009-11-21 23:54:56 +0000

    don't show a title

--- recorder/src/main/java/moten/david/util/tv/ui/client/widget/TitlePanel.java
+++ recorder/src/main/java/moten/david/util/tv/ui/client/widget/TitlePanel.java
@@ -1,13 +1,12 @@
 package moten.david.util.tv.ui.client.widget;
 
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class TitlePanel extends HorizontalPanel {
 
 	public TitlePanel() {
 		setStyleName("title");
-		add(new HTML("<h2>TV Recorder</h2>"));
+		// add(new HTML("<h2>TV Recorder</h2>"));
 	}
 
 }
